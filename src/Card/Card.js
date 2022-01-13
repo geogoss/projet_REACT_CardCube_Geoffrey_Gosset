bd2aca74d49dc3bca9ad94991f1c4aeae312e674
geoffrey <geoffreygosset123@gmail.com>
2022-01-13 16:08:59 +0100

    card et imgCard centré, component créé et fonctionnels

---
+++ src/Card/Card.js
@@ -0,0 +1,9 @@
+import CardImg from "../CardImg/CardImg"
+
+function Card() {
+    return (
+        <CardImg />
+    )
+}
+
+export default Card;
---
+++ src/CardImg/CardImg.js
@@ -0,0 +1,10 @@
+import img from '../images/imgCube.jpg'
+import './CardImg.css'
+
+function CardImg() {
+    return (
+        <img className='carteImage' src={img} alt="" />
+    )
+}
+
+export default CardImg;
